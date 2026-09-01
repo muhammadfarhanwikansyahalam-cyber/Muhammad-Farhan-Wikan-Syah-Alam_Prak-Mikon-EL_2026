2de341592f1837a828de86e690d538b0609c7341
muhammadfarhanwikansyahalam-cyber <muhammadfarhanwikansyahalam@mail.ugm.ac.id>
2026-09-01 22:11:53 +0700

    Initial commit

---
+++ src/hhh.h
@@ -0,0 +1,12 @@
+
+#include <Arduino.h>
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+
+Serial.begin(9600);
+}
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+
+#include <Arduino.h>
+#include "hhh.h"
+// put function declarations here:
+int myFunction(int, int);
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+Serial.println("woy");
+delay(1000);
+Serial.println("Hello, world!");
+
+delay(1000);
+
+
+}
+
