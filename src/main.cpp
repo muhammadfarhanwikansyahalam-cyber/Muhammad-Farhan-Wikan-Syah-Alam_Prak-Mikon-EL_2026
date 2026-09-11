96d32fee7c68579dc9761e143eff6befbea0a901
muhammadfarhanwikansyahalam-cyber <muhammadfarhanwikansyahalam@mail.ugm.ac.id>
2026-09-11 22:23:17 +0700

    tugas kombinasi mikon pertemuan 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,45 @@
-#define potPin 2
+const int potPin = 2;
+const int ledMerah = 25;
+const int ledKuning = 26;
+const int ledHijau = 27;
+const int tombol = 4;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(ledMerah, OUTPUT);
+  pinMode(ledKuning, OUTPUT);
+  pinMode(ledHijau, OUTPUT);
+  pinMode(tombol, INPUT_PULLUP);
 }
 
 void loop() {
   int adcValue = analogRead(potPin);
-  float voltage = (adcValue / 4095.0) * 3.3;
-  float resistance = (voltage / 3.3) * 10000.0;
 
-  Serial.print("ADC       : ");
+  if (digitalRead(tombol) == LOW) {
+    digitalWrite(ledMerah, LOW);
+    digitalWrite(ledKuning, LOW);
+    digitalWrite(ledHijau, LOW);
+  }
+  else {
+    if (adcValue < 1300) {
+      digitalWrite(ledMerah, HIGH);
+      digitalWrite(ledKuning, LOW);
+      digitalWrite(ledHijau, LOW);
+    }
+    else if (adcValue < 2600) {
+      digitalWrite(ledMerah, LOW);
+      digitalWrite(ledKuning, HIGH);
+      digitalWrite(ledHijau, LOW);
+    }
+    else {
+      digitalWrite(ledMerah, LOW);
+      digitalWrite(ledKuning, LOW);
+      digitalWrite(ledHijau, HIGH);
+    }
+  }
+
+  Serial.print("ADC: ");
   Serial.println(adcValue);
 
-  Serial.print("Tegangan  : ");
-  Serial.print(voltage, 2);
-  Serial.println(" V");
-
-  Serial.print("Resistansi: ");
-  Serial.print(resistance, 2);
-  Serial.println(" Ohm");
-
-  delay(500);
+  delay(100);
 }
