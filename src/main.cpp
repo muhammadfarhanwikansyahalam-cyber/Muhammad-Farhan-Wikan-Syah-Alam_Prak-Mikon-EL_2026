ea4901d73676b8254a5f4f1b472e0648803f7718
muhammadfarhanwikansyahalam-cyber <muhammadfarhanwikansyahalam@mail.ugm.ac.id>
2026-09-11 22:15:07 +0700

    potensio pertemuan mikon 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
-#include <Arduino.h>
- 
-#define LED_PIN 2
-#define PUSHBUTTON 5
-
+#define potPin 2
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  
-  pinMode(PUSHBUTTON, INPUT);
 }
- 
+
 void loop() {
+  int adcValue = analogRead(potPin);
+  float voltage = (adcValue / 4095.0) * 3.3;
+  float resistance = (voltage / 3.3) * 10000.0;
 
-  
+  Serial.print("ADC       : ");
+  Serial.println(adcValue);
 
-  if (digitalRead(PUSHBUTTON == LOW)){  
-    digitalWrite(LED_PIN, LOW);
-  } else {
-      digitalWrite(LED_PIN, HIGH);
-  }
+  Serial.print("Tegangan  : ");
+  Serial.print(voltage, 2);
+  Serial.println(" V");
+
+  Serial.print("Resistansi: ");
+  Serial.print(resistance, 2);
+  Serial.println(" Ohm");
+
+  delay(500);
 }
