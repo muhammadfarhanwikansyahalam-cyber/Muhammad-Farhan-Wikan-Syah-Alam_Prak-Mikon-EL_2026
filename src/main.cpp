73f984611214c968d8807c546f356546f0125212
muhammadfarhanwikansyahalam-cyber <muhammadfarhanwikansyahalam@mail.ugm.ac.id>
2026-09-15 20:57:12 +0700

    SENSOR BME PERTEMUAN 2

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,31 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+
 #define SCL_PIN 26
 #define SDA_PIN 14
 
+Adafruit_BME280 bme;
 
 const int pwmPin = 32;
 
 const int freq = 5000;
 const int pwmChannel = 0;
 const int resolution = 8;
-      
 
 void setup() {
   ledcSetup(pwmChannel, freq, resolution);
   ledcAttachPin(pwmPin, pwmChannel);
-  Wire.begin();
-  bme.begin(0x76); // Alamat I2C default BME280
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  bme.begin(0x76);
 }
 
-
 void loop() {
+  float suhu = bme.readTemperature();
 
+  ledcWrite(pwmChannel, suhu * 10);
 
-    float suhu = bme.readTemperature();
-   ledcWrite(pwmChannel, suhu*100);
-    
-    delay(15);
-  }
+  delay(15);
+}
