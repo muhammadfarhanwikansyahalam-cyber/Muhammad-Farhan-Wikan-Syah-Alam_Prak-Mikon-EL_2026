125e6bb635395109936a389b903b51107c8db933
muhammadfarhanwikansyahalam-cyber <muhammadfarhanwikansyahalam@mail.ugm.ac.id>
2026-09-22 04:20:41 +0700

    mpu6050 dan oled

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,37 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_INA219.h>
+#include <Adafruit_MPU6050.h>
+Adafruit_MPU6050 mpu;
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-Adafruit_INA219 ina219;
+
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void setup() {
+  Wire.begin( 21, 22);
 Serial.begin(115200);
 display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-ina219.begin();
  display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
+  mpu.begin();
 }
 
 
 
 
 void loop() {
-float arus = ina219.getCurrent_mA();
-float tegangan_V = ina219.getBusVoltage_V();
-float daya_mW = ina219.getPower_mW();
 
 display.clearDisplay();
 display.setCursor(0,10);
-  display.print (arus);
+
+sensors_event_t a, g, temp;
+mpu.getEvent(&a, &g, &temp);
+
+  display.print (a.acceleration.x);
   display.setCursor(0,20);
-display.print (tegangan_V);
+display.print (a.acceleration.y);
 display.setCursor(0,30);
-display.print (daya_mW);
+display.print (a.acceleration.z);
 display.display();
 delay(1000);
 }
